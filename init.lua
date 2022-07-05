74bf07772a8c78879be8de5600ee23802a7ff161
JOB MONARI <monarijob@yahoo.com>
2022-07-05 03:44:44 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,84 +1,256 @@
--- can be a global or local variable, doesn't matter.
--- name can be anything, eg. you can use `p` or `P` if
--- you prefer it to be something shorter.
-Plug = require('neopm')
---looks
-Plug 'glepnir/spaceline.vim'
-Plug 'ryanoasis/vim-devicons'
-Plug 'dracula/vim'
-Plug 'sainnhe/edge'
-Plug 'nanotech/jellybeans.vim'
-Plug 'arcticicestudio/nord-vim'
-Plug 'cocopon/iceberg.vim'
-Plug 'connorholyday/vim-snazzy'
-Plug 'jacoborus/tender.vim'
-Plug 'tomasiser/vim-code-dark'
-Plug 'luochen1990/rainbow'
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function()
+ -- Packer can manage itself --
+  use 'wbthomason/packer.nvim'
+  
+ -- coc nvim --
+ use {'neoclide/coc.nvim', branch = 'release'}
+ 
+ -- AutoPairs --
+ use 'jiangmiao/auto-pairs'
+ 
+ -- darkPlus --
+ use 'martinsione/darkplus.nvim'
+ 
+ -- Tokyo Night --
+ use 'folke/tokyonight.nvim'
 
---Dev Icons
-Plug 'ryanoasis/vim-devicons'
+ -- Lua Line --
+ use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+ }
+ -- Eviline config for lualine
+-- Author: shadmansaleh
+-- Credit: glepnir
+local lualine = require('lualine')
 
---fzf	
-Plug 'junegunn/fzf'
+-- Color table for highlights
+-- stylua: ignore
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
 
---braces
-Plug 'jiangmiao/auto-pairs'
-Plug 'glepnir/lspsaga.nvim'
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
 
---css color
-Plug 'ap/vim-css-color'
+-- Config
+local config = {
+  options = {
+    -- Disable sections and component separators
+    component_separators = '',
+    section_separators = '',
+    theme = {
+      -- We are going to use lualine_c an lualine_x as left and
+      -- right section. Both are highlighted by c theme .  So we
+      -- are just setting default looks o statusline
+      normal = { c = { fg = colors.fg, bg = colors.bg } },
+      inactive = { c = { fg = colors.fg, bg = colors.bg } },
+    },
+  },
+  sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    -- These will be filled later
+    lualine_c = {},
+    lualine_x = {},
+  },
+  inactive_sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
+}
 
---git
-Plug 'airblade/vim-gitgutter'
-Plug 'rhysd/git-messenger.vim'
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
 
---Languages
-Plug 'fatih/vim-go'
-Plug 'rust-lang/rust.vim'
-Plug 'mattn/emmet-vim'
-Plug 'octol/vim-cpp-enhanced-highlight'
-Plug 'prabirshrestha/vim-lsp'
-Plug 'mattn/vim-lsp-settings'
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
 
---html and shit
-Plug 'valloric/matchtagalways'
+ins_left {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.blue }, -- Sets highlighting of component
+  padding = { left = 0, right = 1 }, -- We don't need space before this
+}
 
---AutoCompletions
-Plug 'racer-rust/vim-racer'
-Plug 'phildawes/racer'
-Plug 'vim-scripts/HTML-AutoCloseTag'
+ins_left {
+  -- mode component
+  function()
+    return ''
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    local mode_color = {
+      n = colors.red,
+      i = colors.green,
+      v = colors.blue,
+      [''] = colors.blue,
+      V = colors.blue,
+      c = colors.magenta,
+      no = colors.red,
+      s = colors.orange,
+      S = colors.orange,
+      [''] = colors.orange,
+      ic = colors.yellow,
+      R = colors.violet,
+      Rv = colors.violet,
+      cv = colors.red,
+      ce = colors.red,
+      r = colors.cyan,
+      rm = colors.cyan,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
+      t = colors.red,
+    }
+    return { fg = mode_color[vim.fn.mode()] }
+  end,
+  padding = { right = 1 },
+}
 
---Wakatime
-Plug 'wakatime/vim-wakatime'
+ins_left {
+  -- filesize component
+  'filesize',
+  cond = conditions.buffer_not_empty,
+}
 
-Plug.load()
-vim.wo.fillchars='eob: '
-vim.cmd([[set background=dark termguicolors number relativenumber]])
-vim.cmd([[colorscheme edge]])
-vim.cmd([[let g:spaceline_seperate_style = 'none']])
-vim.cmd([[let g:spaceline_colorscheme = 'dracula']])
-vim.cmd([[let g:rainbow_active = 1]])
-vim.cmd([[nmap <space>f <Cmd>FZF<CR>]])
-vim.cmd([[if has('termguicolors')
-	let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"
-	let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
-	  set termguicolors
-endif]])
-vim.cmd([[inoremap <expr> <TAB> pumvisible() ? "\<C-y>" : "\<CR>"]])
-vim.cmd([[function! SuperCleverTab()
-	if strpart(getline('.'), 0, col('.') - 1) =~ '^\s*$'
-		return "\<Tab>"
-	elseif pumvisible()
-		return "\<c-n>"
-	else
-		if &omnifunc != ''
-			return "\<C-X>\<C-O>"
-		elseif &dictionary!= ''
-			return "\<C-K>"
-		else
-			return"\<C-N>"
-		endif
-	endif
-endfunction
-inoremap <Tab> <C-R>=SuperCleverTab()<cr>]])
+ins_left {
+  'filename',
+  cond = conditions.buffer_not_empty,
+  color = { fg = colors.magenta, gui = 'bold' },
+}
+
+ins_left { 'location' }
+
+ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+
+ins_left {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  symbols = { error = ' ', warn = ' ', info = ' ' },
+  diagnostics_color = {
+    color_error = { fg = colors.red },
+    color_warn = { fg = colors.yellow },
+    color_info = { fg = colors.cyan },
+  },
+}
+
+-- Insert mid section. You can make any number of sections in neovim :)
+-- for lualine it's any number greater then 2
+ins_left {
+  function()
+    return '%='
+  end,
+}
+
+-- Add components to right sections
+ins_right {
+  'o:encoding', -- option component same as &encoding in viml
+  fmt = string.upper, -- I'm not sure why it's upper case either ;)
+  cond = conditions.hide_in_width,
+  color = { fg = colors.green, gui = 'bold' },
+}
+
+ins_right {
+  'fileformat',
+  fmt = string.upper,
+  icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+  color = { fg = colors.green, gui = 'bold' },
+}
+
+ins_right {
+  'branch',
+  icon = '',
+  color = { fg = colors.violet, gui = 'bold' },
+}
+
+ins_right {
+  'diff',
+  -- Is it me or the symbol for modified us really weird
+  symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.orange },
+    removed = { fg = colors.red },
+  },
+  cond = conditions.hide_in_width,
+}
+
+ins_right {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.blue },
+  padding = { left = 1 },
+}
+
+-- Now don't forget to initialize lualine
+lualine.setup(config)
+
+ 
+ -- ClangFormatAutoEnable
+ use 'octol/vim-cpp-enhanced-highlight'
+ use 'rhysd/vim-clang-format'
+ use 'bfrg/vim-cpp-modern'
+ vim.cmd([[
+  augroup ClangFormatAutoEnable
+    autocmd!
+    autocmd FileType cpp ClangFormatAutoEnable
+  augroup end
+ ]])
+ 
+ -- Ruby --
+ use 'vim-ruby/vim-ruby'
+ use 'tpope/vim-rails'
+ 
+ -- FZF --
+ use {'junegunn/fzf', run = ':call fzf#install()' }
+ use {'junegunn/fzf.vim', depends = 'junegunn/fzf' }
+ vim.cmd([[nmap <space>f <Cmd>FZF<CR>]])
+ 
+ -- wakatime --
+ use 'wakatime/vim-wakatime'
+ 
+ -- Basic cmds --
+ vim.wo.fillchars='eob: '
+ vim.opt.number = true
+ vim.opt.relativenumber = true
+ vim.o.background = "dark"
+ vim.cmd([[colorscheme tokyonight]])
+
+end)
 
