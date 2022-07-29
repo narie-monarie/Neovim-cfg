dfd3f75aca9f10b21db7a884e3c891a55db8469e
JOB MONARI <monarijob@yahoo.com>
2022-07-29 20:07:15 +0300

    Update plugins.lua

--- lua/use/plugins.lua
+++ lua/use/plugins.lua
@@ -106,7 +106,7 @@ return packer.startup(function(use)
  --Telescope FZF --
  use {'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }}
  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
- vim.cmd([[nmap <space>f <Cmd>Telescope find_files<CR>]])
+
 
  -- wakatime --
  use 'wakatime/vim-wakatime'
